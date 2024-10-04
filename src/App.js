8390deb81800628b4de2c1f6cbe61a5c6b12b25d
Kaio Pimentel <kakapimentel59@gmail.com>
2024-10-04 17:52:29 -0300

    finalizando o Projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,131 @@
 
+import Input from "./components/Input"
+import Button from "./components/Button"
+
+import { Container,Content,Row} from "./styles";
+import { useState } from "react";
+
+
 const App = () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState(' ');
+
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
+  };
+
+  const handleAddNumber = (number) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`)
+  };
+
+  const handleSumNumbers = () => {
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('+')
+    }else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum))
+      setOperation('')
+      setFirstNumber('0')
+    }
+  }
+  const handleMinusNumbers = () => {
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('-')
+    }else {
+      const sub = Number(firstNumber) - Number(currentNumber);
+      setCurrentNumber(String(sub))
+      setOperation('')
+      setFirstNumber('0')
+    }
+  }
+  const handleMultiNumbers = () => {
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('*')
+    }else {
+      const multi = Number(firstNumber) * Number(currentNumber);
+      setCurrentNumber(String(multi))
+      setOperation('')
+      setFirstNumber('0')
+    }
+  }
+  const handleDivideNumbers = () => {
+    if(firstNumber === '0'){
+        setFirstNumber(String(currentNumber));
+        setCurrentNumber('0')
+        setOperation('/')
+    }else {
+      const divide = Number(firstNumber) / Number(currentNumber);
+      setCurrentNumber(String(divide))
+      setOperation('')
+      setFirstNumber('0')
+    }
+  }
+
+  const handleEquals = () => {
+    if(firstNumber !== '0' && operation !== ''){
+        switch(operation){
+          case '+':
+          handleSumNumbers();
+          break;
+          default: break;
+
+          case '-':
+          handleMinusNumbers();
+          break;
+          
+
+          case '*':
+          handleMultiNumbers();
+          break;
+          
+
+          case '/':
+          handleDivideNumbers();
+          break;
+          
+        }
+    }
+  }
+
   return (
-    <div className="App">
-      Olá
-    </div>
+    <Container>
+      <Content>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="0" onCLick={() => handleAddNumber('0')}/>
+          <Button label="AC" onCLick={handleOnClear}/>
+          <Button label="/" onCLick={handleDivideNumbers}/>
+          <Button label="X" onCLick={handleMultiNumbers}/>
+        </Row>
+        <Row>
+          <Button label="7" onCLick={() => handleAddNumber('7')}/>
+          <Button label="8" onCLick={() => handleAddNumber('8')}/>
+          <Button label="9" onCLick={() => handleAddNumber('9')}/>
+          <Button label="-" onCLick={handleMinusNumbers}/>
+        </Row>
+        <Row>
+          <Button label="4" onCLick={() => handleAddNumber('4')}/>
+          <Button label="5" onCLick={() => handleAddNumber('5')}/>
+          <Button label="6" onCLick={() => handleAddNumber('6')}/>
+          <Button label="+" onCLick={handleSumNumbers}/>
+        </Row>
+        <Row>
+          <Button label="1" onCLick={() => handleAddNumber('1')}/>
+          <Button label="2" onCLick={() => handleAddNumber('2')}/>
+          <Button label="3" onCLick={() => handleAddNumber('3')}/>
+          <Button label="=" onCLick={handleEquals}/>
+        </Row>
+      </Content>
+    </Container>
   );
 }
 
